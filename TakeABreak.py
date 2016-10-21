110c32928bd7b5b85537238f15377e74524f5cc1
Shreyamsh Kamate <shreyamsh@gmail.com>
2016-10-21 12:55:42 -0700

    Update TakeABreak.py

--- TakeABreak.py
+++ TakeABreak.py
@@ -1,11 +1,11 @@
 import webbrowser
 import time
 
-total_break = 0
+total_break = 3
 break_count = 0
 
 print("Program started on:"+time.ctime())
 while(break_count < total_break):
 	webbrowser.open("https://www.youtube.com/watch?v=cnuAuuXvNPw")
-	time.sleep(10)
-	break_count++
+	time.sleep(20*60)
+	break_count = break_count + 1
